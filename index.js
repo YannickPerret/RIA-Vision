0f191e4b67fd4c2a535b2c8b3786e3d117326d11
Yannick Perret <contact@yannickperret.com>
2023-11-22 15:53:20 +0100

    added terminal color and format data

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const args = process.argv;
   try {
     const base64Data = await Factory.encode(args[2], "base64");
     const data = await AWSClient.parseElement(base64Data);
-    console.log(data);
+
+    for (let i = 0; i < data.Labels.length; i++) {
+      console.log(
+        `Label : \x1b[33m${data.Labels[i].Name}\x1b[0m with confidence \x1b[33m${data.Labels[i].Confidence}\x1b[0m`
+      );
+    }
+    console.log(`MaxLabels : ${data.MaxLabels}`);
+    console.log(`MinConfidence : ${data.MinConfidence}`);
   } catch (err) {
     console.error(err);
   }
