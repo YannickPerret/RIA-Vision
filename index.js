96f0b280890fcc87b8a82af62c8ba9cadd4bd2d3
Yannick Perret <contact@yannickperret.com>
2023-12-06 15:55:44 +0100

    added sharp to convert webp to png

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Factory = require("./lib/factory/factory");
 const VisionProvider = require("./lib/visionProvider");
 const { db, r } = require("./lib/database/database");
 
+
 const args = process.argv;
 
 let maxLabels = Number(args[3]) || undefined;
@@ -9,9 +10,7 @@ let minConfidence = Number(args[4]) || undefined;
 
 // create commande -?
 if (args[2] === "--help") {
-  console.log(
-    "\x1b[33mUsage: node index.js : <provider> <image> <maxLabels> <minConfidence>\x1b[0m"
-  );
+  console.log("\x1b[33mUsage: node index.js : <provider> <image> <maxLabels> <minConfidence>\x1b[0m");
   return;
 }
 
@@ -30,15 +29,9 @@ if (args[2] === "--help") {
     });
     const base64Data = await Factory.encode(args[2], "base64");
     if (!maxLabels || !minConfidence) {
-      throw new Error(
-        "No maxLabels or minConfidence or label provided, using default values"
-      );
+      throw new Error("No maxLabels or minConfidence or label provided, using default values");
     }
-    const data = await Vision.parseElement(
-      base64Data,
-      maxLabels,
-      minConfidence
-    );
+    const data = await Vision.parseElement( base64Data, maxLabels, minConfidence);
     console.log(data.Labels.length);
 
     if (data.Labels.length === 0) {
@@ -49,9 +42,7 @@ if (args[2] === "--help") {
     await db.insert("image", data);
 
     for (let i = 0; i < data.Labels.length; i++) {
-      console.log(
-        `Label : \x1b[33m${data.Labels[i].Name}\x1b[0m with confidence \x1b[33m${data.Labels[i].Confidence}\x1b[0m`
-      );
+      console.log(`Label : \x1b[33m${data.Labels[i].Name}\x1b[0m with confidence \x1b[33m${data.Labels[i].Confidence}\x1b[0m`);
     }
     console.log(`MaxLabels : ${data.MaxLabels}`);
     console.log(`MinConfidence : ${data.MinConfidence}`);
