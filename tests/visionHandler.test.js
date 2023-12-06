fed985614c6987b2402ebe82f4b96bf82d684299
Yannick Perret <contact@yannickperret.com>
2023-12-06 16:20:41 +0100

    fix tests and remove unused file

--- tests/visionHandler.test.js
+++ tests/visionHandler.test.js
@@ -1,4 +1,5 @@
 const VisionProvider = require("../lib/visionProvider");
+const Factory = require("../lib/factory/factory");
 
 describe("VisionProvider", () => {
   it("should correctly initialize with AWS", () => {
@@ -18,13 +19,17 @@ describe("getDataByImage", () => {
   const Vision = VisionProvider.createClient({ cloud: "AWS", region: "eu-central-1", profile: "default" });
 
   it("should return an error if the image is not valid", async () => {
-    const image = "./assets/invalid.jpg";
-    await expect(Vision.parseElement(image, 10, 60)).rejects.toThrow("Request has invalid image format");
+    const image = "./images/tests/invalid.jpg";
+    const base64Data = await Factory.encode(image);
+
+    await expect(Vision.parseElement(base64Data, 10, 60)).rejects.toThrow("Request has invalid image format");
   });
 
   it("should return data if the image is valid", async () => {
-    const image = "./assets/valid.jpg";
-    await expect(Vision.parseElement(image, 10, 60)).resolves.toBeDefined();
+    const image = "./images/tests/valid.jpg";
+    const base64Data = await Factory.encode(image);
+
+    await expect(Vision.parseElement(base64Data, 10, 60)).resolves.toBeDefined();
   });
 });
 
@@ -33,16 +38,19 @@ describe("getLabelsFromImage", () => {
   const Vision = VisionProvider.createClient({ cloud: "AWS", region: "eu-central-1", profile: "default" });
   
   it("should return the correct number of labels for a valid image", async () => {
-    const image = "./assets/valid.jpg";
+    const image = "./images/tests/valid.jpg";
+    const base64Data = await Factory.encode(image);
+
     const maxLabels = 15;
-    const data = await Vision.parseElement(image, maxLabels, 60);
+    const data = await Vision.parseElement(base64Data, maxLabels, 20);
     expect(data.Labels.length).toEqual(maxLabels);
   });
 
   it("should return the correct number of labels for a valid image URL", async () => {
-    const imageUrl = "https://fastly.picsum.photos/id/291/200/300.jpg?hmac=5htP1HYHWPOMv5wbTtTsh6GjRk__SPxuXIv6gHLBHHg";
+    const imageUrl = "https://c0.lestechnophiles.com/www.numerama.com/wp-content/uploads/2022/09/wow-dragonflight-blizzard-1024x576.jpg?avif=1&key=3e271ace";
+    const urlBase64 = await Factory.encode(imageUrl);
     const maxLabels = 10;
-    const data = await Vision.parseElement(imageUrl, maxLabels, 60);
+    const data = await Vision.parseElement(urlBase64, maxLabels, 20);
     expect(data.Labels.length).toEqual(maxLabels);
   });
 });
